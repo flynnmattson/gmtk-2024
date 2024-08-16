5d9803e719f978643beae8b1ddc8380b2bdf8f32
HeartBox12 <maximosity1@gmail.com>
2024-08-16 12:44:31 -0700

    Initial commit to my branch

---
+++ player/player.gd
@@ -0,0 +1,14 @@
+extends RigidBody3D
+
+@export var force:int
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var inVec = Vector2(0, 0) #Input vector. The direction (x, z) the player wants to go.
+	
+	inVec.x += Input.get_action_strength("move_right")
+	inVec.y += Input.get_action_strength("move_down")
+	inVec.x -= Input.get_action_strength("move_left")
+	inVec.y -= Input.get_action_strength("move_up")
+	
+	apply_central_impulse(Vector3(inVec.x, 0, inVec.y) * force * delta)
