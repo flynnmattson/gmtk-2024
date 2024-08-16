34ed5e3e5af19d13434f3a7c395ea090bb8fe78c
HeartBox12 <maximosity1@gmail.com>
2024-08-16 14:48:43 -0700

    Basic enemy movement

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,6 +1,11 @@
 extends CharacterBody3D
 
+@export var speed:int
 @export var target:Node
 
 func _physics_process(delta):
-	var inVec = global_position - target.global_position.normalized #
+	var inVec = (target.global_position - global_position).normalized() #
+	
+	velocity = inVec * speed * delta
+	velocity.y = 0
+	move_and_slide()
