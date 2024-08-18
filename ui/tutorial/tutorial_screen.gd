d892e92e6bee73df5eef75c32dcf89021c776209
HeadRAM <tuckergrindstaff@gmail.com>
2024-08-18 05:22:05 -0700

    added new music

--- ui/tutorial/tutorial_screen.gd
+++ ui/tutorial/tutorial_screen.gd
@@ -3,8 +3,10 @@ class_name TutorialScreen
 
 
 @onready var video_stream_player: VideoStreamPlayer = %VideoStreamPlayer
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 
 func enable() -> void:
 	visible = true
 	video_stream_player.play()
+	audio_stream_player.play()
